cf3050fd02ebf97be99250f32c3684057b461e45
VIKRANT RAJPUT <VIKRANT RAJPUT>
2017-02-01 11:38:06 +0530

    vikrant

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = '';
-$config['base_url'] = 'http://localhost/challwe/challwe_new/';
+$config['base_url'] = 'http://localhost/challwe_new/';
 //$config['base_url']	= 'https://challwe.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
